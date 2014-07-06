f716b69363ba0a55d4cbd6d6a5624e50a13e6c8d
Sylvain Petreolle <spetreolle@yahoo.fr>
2014-07-06 21:08:36 +0000

    AHK unix tests compiler: Add a startup message. Set executable. Copy tests with original attributes and dates.
    svn path=/trunk/ahk_tests/; revision=2124

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 if [ x"$WINE"x == "xx" ] ; then WINE=$(which wine) ; fi
 if [  ! -x "$WINE" ] ; then echo Wine was not found in path, please install or set WINE variable. ; exit 1; fi
 if [ ! -f Compiler/Ahk2Exe.exe ] ; then echo Please add AHK compiler here ; exit 1 ; fi
-read
+read -p "Setup checks done. Press enter to start build."
 #get all the tests
 TESTSLIST=$( find . -name zz\*.ahk -exec $WINE winepath -w {} \; )
 #avoids being bothered by spaces in file names
@@ -12,4 +12,4 @@ IFS=$(echo -en "\n\b")
 for TEST in $TESTSLIST ; do
   "$WINE" Compiler/Ahk2Exe.exe /in "$TEST" /out "${TEST%.ahk}.exe"
 done
-find . -wholename ./AHK-Tests -prune -o -name zz_*test.exe -exec cp {} AHK-Tests/ \;
+find . -wholename ./AHK-Tests -prune -o -name zz_*test.exe -exec cp -a {} AHK-Tests/ \;
