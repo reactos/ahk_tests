4cc64541e173f8c1a54806db4ed8b7ee2184fc17
Sylvain Petreolle <spetreolle@yahoo.fr>
2016-10-01 17:25:29 +0000

    [AHK] Remove compile prompt.
    svn path=/trunk/ahk_tests/; revision=2293

--- compile.sh
+++ compile.sh
@@ -3,7 +3,6 @@
 if [ x"$WINE"x == "xx" ] ; then WINE=$(which wine) ; fi
 if [  ! -x "$WINE" ] ; then echo Wine was not found in path, please install or set WINE variable. ; exit 1; fi
 if [ ! -f Compiler/Ahk2Exe.exe ] ; then echo Please add AHK compiler here ; exit 1 ; fi
-read -p "Setup checks done. Press enter to start build."
 # get all the tests
 TESTSLIST=$( find . -name zz\*.ahk -exec $WINE winepath -w {} \; )
 # avoids being bothered by spaces in file names
