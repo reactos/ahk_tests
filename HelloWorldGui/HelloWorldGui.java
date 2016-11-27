df1862690818a240e7f057ed1d3712818078f336
Sylvain Petreolle <spetreolle@yahoo.fr>
2016-11-27 21:08:46 +0000

    [AHK] Add source code for HelloWorldGui and HelloWorldNET test applications.
    svn path=/trunk/ahk_tests/; revision=2305

---
+++ HelloWorldGui/HelloWorldGui.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JOptionPane;
+
+public class HelloWorldGui
+{
+
+    public HelloWorldGui()
+    {
+    }
+
+    public static void main(String args[])
+    {
+        JOptionPane.showMessageDialog(null, "Simple Java Hello World GUI application.", "Java GUI app works", 1);
+    }
+}
